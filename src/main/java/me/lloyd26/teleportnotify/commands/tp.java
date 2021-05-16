1600c81baf0b58a37768bfb50b854204546b6009
lloyddominic26 <ademic26@yahoo.com>
2021-05-16 12:30:51 +0800

    Replaced methods, Removed comments

--- src/main/java/me/lloyd26/teleportnotify/commands/tp.java
+++ src/main/java/me/lloyd26/teleportnotify/commands/tp.java
@@ -1,6 +1,7 @@
 package me.lloyd26.teleportnotify.commands;
 
 import me.lloyd26.teleportnotify.TeleportNotify;
+import me.lloyd26.teleportnotify.utils.Utils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -31,18 +32,17 @@ public class tp implements CommandExecutor {
                         Player target = Bukkit.getPlayer(args[0]);
                         String targetName = target.getName();
                         player.teleport(target.getLocation());
-                        //player.sendMessage(ChatColor.YELLOW + "You've teleported to " + ChatColor.DARK_AQUA + targetName);
                         player.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("messages.commands.tp.1player.player").replaceAll("%target%", targetName)));
-                        //target.sendMessage(ChatColor.DARK_AQUA + playerName + ChatColor.YELLOW + " teleported to you");
                         if (target.hasPermission("tpnotify.notify.admin")) {
                             target.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("messages.commands.tp.1player.target").replaceAll("%player%", playerName)));
                         }
                         if (player.hasPermission("tpnotify.notify.admin")) {
-                            //if (player.isOp()) {
-                            //Bukkit.broadcast(ChatColor.translateAlternateColorCodes('&', "&7&o[" + playerName + ": &eteleported to " + targetName + "&7&o]"), "tpnotify.notify.admin");
-                            Bukkit.broadcast(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("messages.commands.tp.1player.staff").replaceAll("%player%", playerName).replaceAll("%target%", targetName)), "tpnotify.notify.admin");
-                            System.out.println(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("messages.commands.tp.1player.staff").replaceAll("%player%", playerName).replaceAll("%target%", targetName)));
-                            //Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&',"&7&o[" + playerName + ": &e&oteleported to " + args[0] + "&7&o]"));
+                            for (Player p : Bukkit.getOnlinePlayers()) {
+                                if (p.hasPermission("tpnotify.notify.admin")) {
+                                    p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("messages.commands.tp.1player.staff").replaceAll("%player%", playerName).replaceAll("%target%", targetName)));
+                                }
+                            }
+                            Utils.broadcastToConsole(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("messages.commands.tp.1player.staff").replaceAll("%player%", playerName).replaceAll("%target%", targetName)));
                         }
                     } else if (args.length == 2) {
                         Player playerToSend = Bukkit.getPlayer(args[0]);
@@ -50,18 +50,12 @@ public class tp implements CommandExecutor {
                         String player1Name = playerToSend.getName();
                         String player2Name = target.getName();
                         playerToSend.teleport(target.getLocation());
-                        //player.sendMessage(ChatColor.YELLOW + "You've teleported " + ChatColor.DARK_AQUA + player1Name + ChatColor.YELLOW + " to " + ChatColor.DARK_AQUA + player2Name);
                         player.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("messages.commands.tp.2player.player").replaceAll("%player1%", player1Name).replaceAll("%player2%", player2Name)));
-                        //playerToSend.sendMessage(ChatColor.YELLOW + "You were teleported to " + ChatColor.DARK_AQUA + player2Name + ChatColor.YELLOW + " by " + ChatColor.DARK_AQUA + playerName);
                         playerToSend.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("messages.commands.tp.2player.player1").replaceAll("%player2%", player2Name).replaceAll("%player%", playerName)));
-                        //target.sendMessage(ChatColor.DARK_AQUA + player1Name + ChatColor.YELLOW + " was teleported to you by " + ChatColor.DARK_AQUA + playerName);
                         target.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("messages.commands.tp.2player.player2").replaceAll("%player1%", player1Name).replaceAll("%player%", playerName)));
                         if (player.hasPermission("tpnotify.notify.admin")) {
-                            //if (player.isOp()) {
-                            //Bukkit.broadcast(ChatColor.translateAlternateColorCodes('&', "&7&o[" + playerName + ": &e&oteleported " + player1Name + " to " + player2Name + "&7&o]"), "tpnotify.notify.admin");
                             Bukkit.broadcast(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("messages.commands.tp.2player.staff").replaceAll("%player%", playerName).replaceAll("%player1%", player1Name).replaceAll("%player2%", player2Name)), "tpnotify.notify.admin");
                             System.out.println(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("messages.commands.tp.2player.staff").replaceAll("%player%", playerName).replaceAll("%player1%", player1Name).replaceAll("%player2%", player2Name)));
-                            //Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&',"&7&o[" + playerName + ": &e&oteleported " + args[0] + " to " + args[1] + "&7&o]"));
                         }
                     } else if (args.length == 3) {
                         final double x = args[0].startsWith("~") ? player.getLocation().getX() + (args[0].length() > 1 ? Double.parseDouble(args[0].substring(1)) : 0) : Double.parseDouble(args[0]);
@@ -75,7 +69,6 @@ public class tp implements CommandExecutor {
                         coordsZ = decimalFormat.format(z);
                         String coords = coordsX + " " + coordsY + " " + coordsZ;
                         player.teleport(loc);
-                        //player.sendMessage(ChatColor.GOLD + "Teleporting...");
                         player.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("messages.commands.tp.coords.player").replaceAll("%coords%", coords)));
                         if (player.hasPermission("tpnotify.notify.admin")) {
                             Bukkit.broadcast(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("messages.commands.tp.coords.staff").replaceAll("%player%", playerName).replaceAll("%coords%", coords)), "tpnotify.ntofiy.admin");
