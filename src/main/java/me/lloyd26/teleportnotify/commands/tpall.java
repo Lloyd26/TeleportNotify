0c68eb985732871e3f648d2929cdc69609c81f41
lloyddominic26 <ademic26@yahoo.com>
2021-05-16 14:53:12 +0800

    Update

--- src/main/java/me/lloyd26/teleportnotify/commands/tpall.java
+++ src/main/java/me/lloyd26/teleportnotify/commands/tpall.java
@@ -1,6 +1,8 @@
 package me.lloyd26.teleportnotify.commands;
 
 import me.lloyd26.teleportnotify.TeleportNotify;
+import me.lloyd26.teleportnotify.utils.Error;
+import me.lloyd26.teleportnotify.utils.Utils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -18,21 +20,23 @@ public class tpall implements CommandExecutor {
             Player player = (Player) sender;
             String playerName = player.getName();
             if (player.hasPermission("tpnotify.tpall.use")) {
-                for (Player p : Bukkit.getServer().getOnlinePlayers()) {
+                for (Player p : Bukkit.getOnlinePlayers()) {
                     p.teleport(player.getLocation());
                     if (p.hasPermission("tpnotify.notify.admin")) {
                         p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("messages.commands.tpall.target").replaceAll("%player%", playerName)));
                     }
                 }
                 if (player.hasPermission("tpnotify.notify.admin")) {
-                    Bukkit.broadcast(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("messages.commands.tpall.staff").replaceAll("%player%", playerName)), "tpnotify.notify.admin");
-                    System.out.println((ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("messages.commands.tpall.staff").replaceAll("%player%", playerName))));
+                    for (Player p : Bukkit.getOnlinePlayers()) {
+                        p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("messages.commands.tpall.staff").replaceAll("%player%", playerName)));
+                    }
+                    Utils.broadcastToConsole(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("messages.commands.tpall.staff").replaceAll("%player%", playerName)));
                 }
             } else {
-                player.sendMessage(ChatColor.DARK_RED + "Error: " + ChatColor.RED + "Insufficient permission!");
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', Utils.getErrorMessage(Error.NOPERMISSION)));
             }
         } else {
-            sender.sendMessage("You need to be a player to execute this command!");
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Utils.getErrorMessage(Error.PLAYERSONLY)));
         }
         return true;
     }
