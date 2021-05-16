f97ca79e0ad91080f166bf0aa86e548833b62745
lloyddominic26 <ademic26@yahoo.com>
2021-05-16 13:03:43 +0800

    Added setUsage(), getPrimaryColor(), and getAccentColor() method

--- src/main/java/me/lloyd26/teleportnotify/utils/Utils.java
+++ src/main/java/me/lloyd26/teleportnotify/utils/Utils.java
@@ -1,6 +1,8 @@
 package me.lloyd26.teleportnotify.utils;
 
 import me.lloyd26.teleportnotify.TeleportNotify;
+import org.apache.commons.lang.Validate;
+import org.bukkit.ChatColor;
 
 public class Utils {
 
@@ -8,5 +10,22 @@ public class Utils {
 
     public static void broadcastToConsole(String message) {
         if (plugin.getConfig().getBoolean("config.broadcast-to-console")) System.out.println(message);
+    }
+
+    public static String setUsage(String usage) {
+        Validate.notNull(usage);
+        Validate.notEmpty(usage);
+        String[] args = usage.split("(?=<)|(?=\\[)", 2);
+        return getPrimaryColor() + "Usage: " + getAccentColor() + args[0] + ChatColor.RESET + args[1];
+    }
+
+    public static ChatColor getPrimaryColor() {
+        Validate.notNull(plugin.getConfig().getString("messages.theme.primary-color"));
+        return ChatColor.valueOf(plugin.getConfig().getString("messages.theme.primary-color"));
+    }
+
+    public static ChatColor getAccentColor() {
+        Validate.notNull(plugin.getConfig().getString("messages.theme.accent-color"));
+        return ChatColor.valueOf(plugin.getConfig().getString("messages.theme.accent-color"));
     }
 }
