c86ef79ef5ad477f4629bb59cb764b520b2770e9
lloyddominic26 <ademic26@yahoo.com>
2021-09-07 22:19:12 +0800

    Improved permission handling for tpnotify command

--- src/main/java/me/lloyd26/teleportnotify/commands/tpnotify.java
+++ src/main/java/me/lloyd26/teleportnotify/commands/tpnotify.java
@@ -1,6 +1,7 @@
 package me.lloyd26.teleportnotify.commands;
 
 import me.lloyd26.teleportnotify.TeleportNotify;
+import me.lloyd26.teleportnotify.utils.Error;
 import me.lloyd26.teleportnotify.utils.Utils;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -14,19 +15,19 @@ public class tpnotify implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (args.length == 1) {
-            if (args[0].equalsIgnoreCase("reload")) {
-                if (sender.hasPermission("tpnotify.reload")) {
-                    plugin.reloadConfig();
-                    sender.sendMessage(Utils.getAccentColor() + "TeleportNotify" + Utils.getPrimaryColor() + " reloaded!");
-                    if (sender instanceof Player) {
-                        Player player = (Player) sender;
-                        plugin.getLogger().info(Utils.getAccentColor() + "TeleportNotify" + Utils.getPrimaryColor() + " reloaded by " + Utils.getAccentColor() + player.getName() + Utils.getPrimaryColor() + "!");
-                    }
+        if (sender.hasPermission("tpnotify.reload")) {
+            if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
+                plugin.reloadConfig();
+                sender.sendMessage(Utils.getAccentColor() + "TeleportNotify" + Utils.getPrimaryColor() + " reloaded!");
+                if (sender instanceof Player) {
+                    Player player = (Player) sender;
+                    plugin.getLogger().info(Utils.getAccentColor() + "TeleportNotify" + Utils.getPrimaryColor() + " reloaded by " + Utils.getAccentColor() + player.getName() + Utils.getPrimaryColor() + "!");
                 }
+            } else {
+                sender.sendMessage(Utils.setUsage("/tpnotify reload"));
             }
         } else {
-            sender.sendMessage(Utils.setUsage("/tpnotify reload"));
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Utils.getErrorMessage(Error.NOPERMISSION)));
         }
         return true;
     }
